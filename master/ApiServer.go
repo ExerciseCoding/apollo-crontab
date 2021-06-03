fa01a4dd04b4c3b8f9e32df633bd34bc898c3776
SunshineDay <924345620@qq.com>
2021-06-03 17:15:39 +0800

    create index.html

--- master/ApiServer.go
+++ master/ApiServer.go
@@ -158,12 +158,23 @@ ERR:
 }
 //初始化服务
 func InitApiServer() (err error) {
+	var(
+		staticDir http.Dir
+		staticHandler http.Handler
+	)
 	//配置路由
 	mux := http.NewServeMux()
 	mux.HandleFunc("/cron/job/save", handleJobSave)
 	mux.HandleFunc("/cron/job/delete", handleJobDelete)
 	mux.HandleFunc("/cron/job/list",handleJobList)
-	mux.HandleFunc("/job/kill",handleJobKill)
+	mux.HandleFunc("/cron/job/kill",handleJobKill)
+	// 首页请求路由: /index.html
+	//静态文件目录
+	staticDir = http.Dir(G_config.WebRoot)
+
+	staticHandler = http.FileServer(staticDir)
+	//匹配到/index.html后会去掉/然后加上./webroot形成./webroot/index.html
+	mux.Handle("/",http.StripPrefix("/",staticHandler))
 	//启动tcp监听地址和端口
 	listener, err := net.Listen("tcp", ":"+strconv.Itoa(G_config.ApiPort))
 	if err != nil {
