29892fd01f764b74c5bdd3674afb360068a0c3e1
SunshineDay <924345620@qq.com>
2021-06-07 16:05:17 +0800

    work watch jobs

--- common/Protocol.go
+++ common/Protocol.go
@@ -1,6 +1,9 @@
 package common
 
-import "encoding/json"
+import (
+	"encoding/json"
+	"strings"
+)
 
 //定时任务
 type CronJob struct {
@@ -15,7 +18,11 @@ type Response struct {
 	Msg string `json:"msg"`
 	Data interface{} `json:"data"`
 }
-
+//变化事件
+type JobEvent struct {
+	EventType int
+	job *CronJob
+}
 //应答方法
 func BuildResponse(errno int,msg string,data interface{})(resp []byte,err error){
 	//1.定义一个Response
@@ -29,4 +36,28 @@ func BuildResponse(errno int,msg string,data interface{})(resp []byte,err error)
 	//序列化
 	resp,err = json.Marshal(response)
 	return
+}
+
+//反序列化Job
+func UnpackJob(jobValue []byte)(job *CronJob,err error){
+	job = &CronJob{}
+	if err = json.Unmarshal(jobValue,job); err != nil {
+		return
+	}
+	return
+}
+
+//从etcd的key中提取任务名
+//例：从/cron/jobs/job10 中提取到job10
+func ExtractJobName(jobKey string)(string){
+	return strings.TrimPrefix(jobKey,JOB_SAVE_DIR)
+}
+
+
+//任务变化事件 1.更新任务 2.删除任务
+func BuildJobEvent(eventType int,job *CronJob)(jobEvent *JobEvent){
+	return &JobEvent{
+		EventType:eventType,
+		job:job,
+	}
 }
--- worker/JobMgr.go
+++ worker/JobMgr.go
@@ -1,6 +1,9 @@
 package worker
 
 import (
+	"context"
+	"crontab/common"
+	"github.com/coreos/etcd/mvcc/mvccpb"
 	"time"
 
 	"github.com/coreos/etcd/clientv3"
@@ -11,12 +14,83 @@ type JobMgr struct {
 	client *clientv3.Client
 	kv     clientv3.KV
 	lease  clientv3.Lease
+	watcher clientv3.Watcher
 }
 
 var (
 	//单例
 	G_jobMgr *JobMgr
 )
+
+func(jobMgr *JobMgr) watchJobs() (err error){
+	var (
+		getResp *clientv3.GetResponse
+		kvpair *mvccpb.KeyValue
+		job *common.CronJob
+		watchStartRevision int64
+		watchChan clientv3.WatchChan
+		watchResp clientv3.WatchResponse
+		watchEvent *clientv3.Event
+		jobName string
+		jobEvent *common.JobEvent
+	)
+	//1.get /cron/jobs/目录下的所有任务，并且获知当前集群的revision
+	if getResp,err = jobMgr.kv.Get(context.TODO(),common.JOB_SAVE_DIR,clientv3.WithPrefix()); err != nil {
+		return
+	}
+
+	for  _, kvpair = range getResp.Kvs {
+		//反序列化json得到Job
+		if job ,err = common.UnpackJob(kvpair.Value); err == nil {
+
+			jobEvent = common.BuildJobEvent(common.JOB_EVENT_SAVE,job)
+			//TODO: 将任务同步给scheduler(调度协程)
+		}
+
+
+	}
+
+	//2.从该revision向后监听变化事件
+	go func(){//监听协程
+
+		//从GET时刻的后续版本开始监听变化
+		watchStartRevision = getResp.Header.Revision + 1
+
+		//启动监听/cron/jobs/目录的后续变化
+		watchChan = jobMgr.watcher.Watch(context.TODO(),common.JOB_SAVE_DIR,clientv3.WithRev(watchStartRevision))
+
+		//处理监听事件
+		for watchResp = range watchChan{
+			for watchEvent = range watchResp.Events{
+				switch watchEvent.Type {
+				case mvccpb.PUT: //任务保存事件
+					if job,err = common.UnpackJob(watchEvent.Kv.Value); err != nil{
+						continue
+					}
+					//构造EVENT
+					jobEvent = common.BuildJobEvent(common.JOB_EVENT_SAVE,job)
+
+				case mvccpb.DELETE: //任务删除事件
+					//Delete /cron/jobs/job10
+					jobName = common.ExtractJobName(string(watchEvent.Kv.Key))
+					//构造一个Event
+					job = &common.CronJob{
+						Name:     jobName,
+					}
+					jobEvent = common.BuildJobEvent(common.JOB_EVENT_DELETE,job)
+
+					
+				
+				}
+
+				//TODO: 向scheduler推送事件
+				//G_Scheduler.PushJobEvent(jobEvent)
+			}
+		}
+
+	}()
+	return
+}
 
 //初始化管理器
 func InitJobMgr() (err error) {
@@ -25,6 +99,7 @@ func InitJobMgr() (err error) {
 		client *clientv3.Client
 		kv     clientv3.KV
 		lease  clientv3.Lease
+		watcher clientv3.Watcher
 	)
 	//初始化配置
 	config = clientv3.Config{
@@ -40,12 +115,17 @@ func InitJobMgr() (err error) {
 	//获取KV和Lease的API子集
 	kv = clientv3.NewKV(client)
 	lease = clientv3.NewLease(client)
+	watcher = clientv3.NewWatcher(client)
 
 	//赋值单例
 	G_jobMgr = &JobMgr{
 		client: client,
 		kv:     kv,
 		lease:  lease,
+		watcher: watcher,
 	}
+
+	//启动任务监听
+	G_jobMgr.watchJobs()
 	return
 }
