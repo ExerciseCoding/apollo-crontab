8b5468a9077d40868ad41ec48f2d586f4a581d29
SunshineDay <924345620@qq.com>
2021-06-17 22:29:29 +0800

    job lock finshed

--- worker/Executor.go
+++ worker/Executor.go
@@ -36,20 +36,33 @@ func (executor *Executor) ExecuteJob(info *common.JobExecuteInfo){
 
 		//任务开始时间
 		result.StartTime = time.Now()
+		err = jobLock.TryLock()
+		defer jobLock.Unlock()
+		if err != nil { //上锁失败
+			result.Err = err
+			result.EndTime = time.Now()
+		}else{
+			//上锁成功后，重置任务启动时间
+			result.StartTime = time.Now()
 
-		//执行crontab的shell命令
-		cmd = exec.CommandContext(context.TODO(),"/bin/bash","-c",info.Job.Command)
-		//执行并捕获输出
-		output,err = cmd.CombinedOutput()
+			//执行crontab的shell命令
+			cmd = exec.CommandContext(context.TODO(),"/bin/bash","-c",info.Job.Command)
+			//执行并捕获输出
+			output,err = cmd.CombinedOutput()
 
-		//任务结束时间
-		result.EndTime = time.Now()
+			//任务结束时间
+			result.EndTime = time.Now()
 
-		result.Output = output
-		result.Err = err
-		
-		//任务执行完成后，把执行的结果返回给scheduler, Scheduler会从executing Table中删除掉执行记录
-		G_scheduler.PushJobResult(result)
+			result.Output = output
+			result.Err = err
+
+			//任务执行完成后，把执行的结果返回给scheduler, Scheduler会从executing Table中删除掉执行记录
+			G_scheduler.PushJobResult(result)
+
+
+		}
+
+
 	}()
 }
 
--- worker/JobLock.go
+++ worker/JobLock.go
@@ -2,6 +2,7 @@ package worker
 
 import (
 	"context"
+	"crontab/common"
 
 	"github.com/coreos/etcd/clientv3"
 )
@@ -11,7 +12,8 @@ type JobLock struct {
 	lease clientv3.Lease
 	jobName string //任务名
 	cancelFunc context.CancelFunc //用于终止自动续租
-
+	leaseId clientv3.LeaseID //租约ID
+	isLocked bool //是否上锁成功
 }
 
 //初始化锁
@@ -25,13 +27,16 @@ func InitJobLock(jobName string,kv clientv3.KV, lease clientv3.Lease)(jobLock *J
 }
 
 //尝试上锁
-func (jobLock *JobLock) TryLock(err error){
+func (jobLock *JobLock) TryLock()(err error){
 	var (
 		leaseGrantResp *clientv3.LeaseGrantResponse
 		cancelCtx context.Context
 		cancelFunc context.CancelFunc
 		leaseId clientv3.LeaseID
 		keepRespChan <- chan *clientv3.LeaseKeepAliveResponse
+		txn clientv3.Txn
+		lockKey string
+		txnResp *clientv3.TxnResponse
 	)
 	//1.创建租约(5秒)
 	if leaseGrantResp,err = jobLock.lease.Grant(context.TODO(),5); err != nil{
@@ -64,13 +69,41 @@ func (jobLock *JobLock) TryLock(err error){
 		END:
 	}()
 	//3.创建事务txn
+	txn = jobLock.kv.Txn(context.TODO())
 
+	//锁路径
+	lockKey = common.JOB_LOCK_DIR + jobLock.jobName
 	//4.事务抢锁
 
-	//5.成功返回，失败释放租约
+	txn.If(clientv3.Compare(clientv3.CreateRevision(lockKey),"=",0)).
+		Then(clientv3.OpPut(lockKey,"",clientv3.WithLease(leaseId))).Else(clientv3.OpGet(lockKey))
 
+	//提交事务
+	if txnResp,err  = txn.Commit(); err != nil{
+		goto FAIL
+	}
+	//6.成功返回，失败释放租约
+	if !txnResp.Succeeded{ //锁被占用
+		err = common.ERR_LOCK_ALREADY_REQUIRED
+		goto FAIL
+
+	}
+	//抢锁成功
+	jobLock.leaseId = leaseId
+	jobLock.cancelFunc = cancelFunc
+	jobLock.isLocked = true
+	return
 FAIL:
 	cancelFunc() //取消自动续租
 	jobLock.lease.Revoke(context.TODO(),leaseId) //释放租约
 	return
+}
+
+//释放锁
+func (jobLock *JobLock) Unlock(){
+	if jobLock.isLocked{
+		jobLock.cancelFunc() //取消程序自动续租的协程
+		jobLock.lease.Revoke(context.TODO(),jobLock.leaseId) //释放租约
+	}
+
 }
